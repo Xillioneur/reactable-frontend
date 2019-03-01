dbc12ddce9039464e566f13d0f22e2a7af16d316
SlickJohnson <liwa.johnson@gmail.com>
2019-02-28 17:52:32 -0800

    Move env config to envs.js

--- config/webpack/webpack.common.babel.js
+++ config/webpack/webpack.common.babel.js
@@ -1,21 +1,9 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import dotenv from 'dotenv';
 
+import envs from './envs';
 import paths from './paths';
 import rules from './rules';
-
-// call dotenv and it will return an Object with a parsed key
-const env = dotenv.config().parsed;
-let envKeys = {};
-
-if (env) {
-  // reduce it to a nice object, the same as before
-  envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-}
 
 module.exports = {
   entry: paths.entryPath,
@@ -40,6 +28,6 @@ module.exports = {
         removeAttributeQuotes: true,
       },
     }),
-    new webpack.DefinePlugin(envKeys),
+    new webpack.DefinePlugin(envs),
   ],
 };
